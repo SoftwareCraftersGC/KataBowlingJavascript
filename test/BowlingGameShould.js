c25e03463dadbd123b4e830d7e566a358c7ebc39
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-03-18 19:03:04 +0000

    green: have a score of 21 when the first 2 rolls are a spare

--- test/BowlingGameShould.js
+++ test/BowlingGameShould.js
@@ -2,24 +2,72 @@
 let should = require('chai').should();
 
 describe('BowlingGame Should', () => {
-  it('have a score of 2 when 2 pins are rolled', () => {
+  it('have a score of 5 when 1 one pin is rolled and then 4 pins are rolled', () => {
     let bowlingGame = BowlingGame();
-    bowlingGame.roll(2);
-    bowlingGame.score().should.equal(2);
+    bowlingGame.roll(1);
+    bowlingGame.roll(4);
+    bowlingGame.score().should.equal(5);
   });
 
-  it('have a score of 4 when 2 pints are rolled and then another 2 pins are rolled', () => {
+  it('have a score of 21 when the first 2 rolls are a spare', () => {
     let bowlingGame = BowlingGame();
-    bowlingGame.roll(2);
-    bowlingGame.roll(2);
-    bowlingGame.score().should.equal(4);
+    bowlingGame.roll(6);
+    bowlingGame.roll(4);
+    bowlingGame.roll(4);
+    bowlingGame.roll(3);
+    bowlingGame.score().should.equal(21);
   });
+
 });
 
 function BowlingGame() {
-  let score = 0;
+  let frame = Frame();
   return {
-    roll : pins => score += pins,
-    score : () => score
+    roll : pins => {
+      frame.roll(pins);
+    },
+    score : () => {
+      return frame.score();
+    }
+  };
+}
+
+function Frame() {
+  let rolledPins = [];
+  let nextFrame;
+  function roll(pins) {
+    if (rolledPins.length == 2) {
+      if (!nextFrame) nextFrame = Frame();
+      nextFrame.roll(pins);
+    } else {
+      rolledPins.push(pins);
+    }
+  }
+
+  function score() {
+    let score = rolledPins.reduce((first, second) => first + second, 0);
+    if (nextFrame) {
+      if (isSpare()) score += nextFrame.first();
+      score += nextFrame.score();
+    }
+    return score;
+  }
+
+  function isSpare() {
+    return first() + second() === 10;
+  }
+
+  function first() {
+    return rolledPins[0];
+  }
+
+  function second() {
+    return rolledPins[1];
+  }
+
+  return {
+    roll : roll,
+    first: first,
+    score : score
   };
 }
