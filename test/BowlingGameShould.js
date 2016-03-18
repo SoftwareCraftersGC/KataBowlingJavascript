2ee74815f04639cc5588e7d04f802ae907b5203f
Miguel Angel Viera Gonzalez <mangelviera93@gmail.com>
2016-03-18 20:00:09 +0000

    Refactor: clean tests

--- test/BowlingGameShould.js
+++ test/BowlingGameShould.js
@@ -13,23 +13,20 @@ describe('BowlingGame Should', () => {
   });
 
   it('have a score of 5 when 1 one pin is rolled and then 4 pins are rolled', () => {
-    bowlingGame.roll(1);
-    bowlingGame.roll(4);
+    const rolls = [1, 4];
+    rolls.forEach(bowlingGame.roll);
     bowlingGame.score().should.equal(5);
   });
 
   it('have a score of 21 when the first 2 rolls are a spare', () => {
-    bowlingGame.roll(6);
-    bowlingGame.roll(4);
-    bowlingGame.roll(4);
-    bowlingGame.roll(3);
+    const rolls = [6, 4, 4, 3];
+    rolls.forEach(bowlingGame.roll);
     bowlingGame.score().should.equal(21);
   });
 
   it('have a score of 24 when the first roll is a strike and the following ones are normal', () => {
-    bowlingGame.roll(10);
-    bowlingGame.roll(4);
-    bowlingGame.roll(3);
+    const rolls = [10, 4, 3];
+    rolls.forEach(bowlingGame.roll);
     bowlingGame.score().should.equal(24);
   });
 
